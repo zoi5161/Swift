60e69f8af4ad55fd72ad0b102b769531db93a22f
Trần Gia Bảo <68107603+zoi5161@users.noreply.github.com>
2024-10-17 15:38:08 +0700

    Add files via upload

---
+++ I am Poor/I am Poor/I am Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Poor
+//
+//  Created by macOS on 11/11/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
